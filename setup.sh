33eb40a79b91c5423989aeb171a59276ce57339c
Parikshit Kumar <parikshit69420@gmail.com>
2020-11-20 21:01:20 +0530

    another commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
+mkdir -p ~/.streamlit
 
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"parikshit69420@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
